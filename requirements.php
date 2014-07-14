2b026e0458d3cb72422ae196ba47671ceed3a645
Joshua Estes <josh@bitpay.com>
2014-07-14 12:13:33 -0400

    Updated the requirements.php file and put a description of what it is and what it does

---
+++ requirements.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * This file is should be ran via the command line and provides useful output
+ * to make sure that the server meets the requirements for this plugin as well
+ * as provide information that is useful to the BitPay support staff.
+ */
+
